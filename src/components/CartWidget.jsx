c929df75d61f3cd02d3dad03b8ab511418cb5279
Rodolfo Perez <rodolfofperezl@gmail.com>
2024-06-04 22:08:59 -0400

    upload

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,3 +1,4 @@
+import ItemListContainer from "./ItemListContainer";
 
 
 const cart = "https://cdn-icons-png.flaticon.com/512/1170/1170678.png";
@@ -6,6 +7,7 @@ export default function CartWidget() {
   return (
     <div>
       <img src={cart} alt="cart" />
+      <ItemListContainer greeting="5" />
     </div>
   );
 }
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+
+
+export default function ItemListContainer({ greeting }) {
+    return (
+        <div>
+            <h1>{greeting}</h1>
+        </div>
+    )
+}
